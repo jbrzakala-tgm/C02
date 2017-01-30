ffe3c7414ca6c91b7ef52e1778e7a36ebe311e9e
jbrzakala <jbrzakala@student.tgm.ac.at>
2017-01-30 10:45:53 +0100

    2version

---
+++ src/GIT/julian.java
@@ -0,0 +1,10 @@
+package GIT;
+
+public class julian {
+
+	public static void main(String[] args){
+		System.out.println("Hallo Julian");
+		System.out.println("Hallo Julian");
+		System.out.println("Hallo Julian");
+	}
+}
